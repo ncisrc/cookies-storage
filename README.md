a9ec30da1a13c7d9f4ceb1c8814a20067f4f8324
Mathieu LALLEMAND <mathieu.lallemand@nci.fr>
2022-04-08 21:25:33 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Cookies Storage
+
+Have you ever dreamed to use Cookies like you use LocalStorage ? hum? 
+And If I say that this lib is very light ? hum ?
+
+Happy Cookie Storage !
+
+## Install 
+```bash
+npm i @ncisrc/cookies-storage
+```
+
+
---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * This is a simple library to work with cookies like with localStorage.
+ *
+ * Author: Mathieu LALLEMAND
+ */
+export const cookieManager = {
+
+  setItem(key, value="", days=1) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    document.cookie = `${key}=${value}; expires=${date.toUTCString()}; path=/`;
+  },
+
+  getItem(key) {
+    const cookies = document.cookie.split(';');
+    const result  = cookies.find(str => str.substring(0,key.length + 1) == `${key}=`);
+    if (result) {
+      const firstSplit = result.indexOf('=') + 1;
+      return result.substring(firstSplit);
+    }
+    return null;
+  },
+
+  removeItem(key) {
+    document.cookie = `${key}=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+  }
+
+}
