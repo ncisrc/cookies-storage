bd95d09bcfe5f7dc2d11ca403c32c4dc5dbd9cfa
Mathieu LALLEMAND <2410437+lalmat@users.noreply.github.com>
2022-04-22 15:25:46 +0200

    add cookies options

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@
  */
 export const cookiesStorage = {
 
-  setItem(key, value="", days=1) {
-    const date = new Date();
-    date.setTime(date.getTime() + (days*24*60*60*1000));
-    document.cookie = `${key}=${value}; expires=${date.toUTCString()}; path=/`;
+  setItem(key, value="", options) {
+    const cookieString = `${key}=${value}` + getOptionsString(options);
+    console.log(cookieString);
+    document.cookie = cookieString;
   },
 
   getItem(key) {
@@ -25,4 +25,48 @@ export const cookiesStorage = {
     document.cookie = `${key}=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
   }
 
+}
+
+/**
+ * Return the default option string according to the given options
+ * @param {object} options
+ * @returns string
+ */
+function getOptionsString(options) {
+  let str = "; ";
+  options = defaultOptions(options);
+  if (options.maxAge)   str += `Max-Age=${options.expire}; `;
+  if (options.sameSite) str += `SameSite=${options.sameSite}; `;
+  if (options.path)     str += `Path=${options.path}; `;
+  if (options.secure)   str += `Secure; `;
+  if (options.httpOnly) str += `HttpOnly; `;
+  if (options.domain)   str += `Domain=${options.domain}; `;
+  return str;
+}
+
+/**
+ * Set the default options if missing
+ * @param {object} options
+ * @returns object
+ */
+function defaultOptions(options) {
+  const defaultOptions = {
+      maxAge   : 36288000,     // 7 Days
+      sameSite : 'Strict',     // Strict
+      path     : '/',          // Only root
+      secure   : true,         // Only HTTPS
+      httpOnly : false,        // Allow javascript to read Cookies
+      domain   : null          // Localdomain only
+    };
+
+  if (!options) return defaultOptions;
+
+  if (!options.hasOwnProperty('maxAge'))   options.maxAge   = defaultOptions.maxAge;
+  if (!options.hasOwnProperty('sameSite')) options.sameSite = defaultOptions.sameSite;
+  if (!options.hasOwnProperty('path'))     options.path     = defaultOptions.path;
+  if (!options.hasOwnProperty('secure'))   options.secure   = defaultOptions.secure;
+  if (!options.hasOwnProperty('httpOnly')) options.domain   = defaultOptions.httpOnly;
+  if (!options.hasOwnProperty('domain'))   options.domain   = defaultOptions.domain;
+
+  return options;
 }
